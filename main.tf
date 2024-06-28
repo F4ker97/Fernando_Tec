9abd2ecd56797c0b72ac03a8862fbe466905e79d
F4ker97 <166553309+F4ker97@users.noreply.github.com>
2024-06-27 22:23:00 -0400

    main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.53.0" # Ajusta la versión según tus necesidades
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc" {
+module "my_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
@@ -26,6 +17,63 @@ module "vpc" {
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = "prd"
   }
+}
+
+resource "aws_security_group" "my_sg" {
+  name        = "my-security-group"
+  description = "Allow HTTP, HTTPS, and SSH traffic"
+  vpc_id      = module.my_vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "my-security-group"
+    Terraform   = "true"
+    Environment = "prd"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
+
+  tags = {
+    Name        = "my-bucket"
+    Environment = "prd"
+  }
+}
+
+resource "aws_s3_bucket_object" "index_php" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "index.php"
+  source = "path/to/your/local/index.php"
+  acl    = "public-read"
 }
